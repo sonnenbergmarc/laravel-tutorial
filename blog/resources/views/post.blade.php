a5d9b47b078ddfc8820094a2e6ba682cbb6ce067
sonnenbergmarc <msonnenberg@live.com>
2023-09-19 20:25:28 -0600

    Create new view and route. Hardcoded

---
+++ blog/resources/views/post.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, temporibus quibusdam dolor error itaque tenetur, vitae libero voluptate minima provident possimus ab voluptas aliquid delectus quo consequatur sit labore blanditiis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, cum aperiam doloremque, expedita nesciunt, iusto ex tenetur maiores distinctio nobis cumque? Molestias eligendi praesentium nostrum et aspernatur blanditiis nulla delectus! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt voluptates non officiis optio corporis laudantium doloremque expedita dolorem rem ut iure, similique neque porro nulla debitis! Dolor sapiente dignissimos consequatur?</p>
+    </article>
+
+    <a href="/">Go Back</a>
+</body>
