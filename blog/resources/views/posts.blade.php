d73236a679ed30845fc300389332f358de3e2acc
sonnenbergmarc <msonnenberg@live.com>
2023-09-19 20:57:28 -0600

    Making posts more dynamic and titles clickable

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -5,18 +5,18 @@
 
 <body>
     <article>
-        <h1><a href="/post">My First Post</a></h1>
+        <h1><a href="/posts/my-first-post">My First Post</a></h1>
 
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, temporibus quibusdam dolor error itaque tenetur, vitae libero voluptate minima provident possimus ab voluptas aliquid delectus quo consequatur sit labore blanditiis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, cum aperiam doloremque, expedita nesciunt, iusto ex tenetur maiores distinctio nobis cumque? Molestias eligendi praesentium nostrum et aspernatur blanditiis nulla delectus! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt voluptates non officiis optio corporis laudantium doloremque expedita dolorem rem ut iure, similique neque porro nulla debitis! Dolor sapiente dignissimos consequatur?</p>
     </article>
     <article>
-        <h1><a href="/post">My Second Post</a></h1>
+        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
 
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, temporibus quibusdam dolor error itaque tenetur, vitae libero voluptate minima provident possimus ab voluptas aliquid delectus quo consequatur sit labore blanditiis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, cum aperiam doloremque, expedita nesciunt, iusto ex tenetur maiores distinctio nobis cumque? Molestias eligendi praesentium nostrum et aspernatur blanditiis nulla delectus! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt voluptates non officiis optio corporis laudantium doloremque expedita dolorem rem ut iure, similique neque porro nulla debitis! Dolor sapiente dignissimos consequatur?</p>
     </article>
 
     <article>
-        <h1><a href="/post">My Third Post</a></h1>
+        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
 
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, temporibus quibusdam dolor error itaque tenetur, vitae libero voluptate minima provident possimus ab voluptas aliquid delectus quo consequatur sit labore blanditiis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, cum aperiam doloremque, expedita nesciunt, iusto ex tenetur maiores distinctio nobis cumque? Molestias eligendi praesentium nostrum et aspernatur blanditiis nulla delectus! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt voluptates non officiis optio corporis laudantium doloremque expedita dolorem rem ut iure, similique neque porro nulla debitis! Dolor sapiente dignissimos consequatur?</p>
     </article>
