c26b9de76a83c07eaa6dbb1693cafc2ec7e6c06a
sonnenbergmarc <msonnenberg@live.com>
2023-09-21 20:48:11 -0600

    Using an array map to get the blog posts in a loop

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -15,8 +15,11 @@ use App\Models\Post;
 |
 */
 
-Route::get('/', function () {
-    return view('posts');
+Route::get('/', function () { 
+    
+    return view('posts', [
+        'posts' => Post::all()
+    ]);
 });
 
 Route::get('posts/{post}', function ($slug) {    
