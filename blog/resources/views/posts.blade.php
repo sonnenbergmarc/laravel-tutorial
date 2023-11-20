8e63aaec6b28fa9149dc6731e79ddf378924aaae
sonnenbergmarc <msonnenberg@live.com>
2023-11-20 06:29:47 -0700

    Created blade component for layout

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -1,20 +1,15 @@
-<!doctype html>
+<x-layout>
+@foreach ($posts as $post)
+    <article class="{{ $loop->even ? 'foobar' : '' }}">
+        <h1>
+            <a href="/posts/{{ $post->slug }}">
 
-<title>My Blog</title>
-<link rel="stylesheet" href="/app.css">
-
-<body>
-    @foreach ($posts as $post)
-        <article class="{{ $loop->even ? 'foobar' : '' }}">
-            <h1>
-                <a href="/posts/{{ $post->slug }}">
-
-                    {{ $post->title }}
-                </a>
-            </h1>
-            <div>
-                {{ $post->excerpt }}
-            </div>
-        </article>
-    @endforeach
-</body>
+                {{ $post->title }}
+            </a>
+        </h1>
+        <div>
+            {{ $post->excerpt }}
+        </div>
+    </article>
+@endforeach
+</x-layout>
