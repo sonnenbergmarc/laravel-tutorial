f1cf74377b6e8eca3114d2597deaf43a6d5d7bb8
sonnenbergmarc <msonnenberg@live.com>
2023-11-20 06:17:37 -0700

    Creating blades and using short hand statements

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -4,16 +4,17 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
-    <?php foreach ($posts as $post) :?>
-    <article>
-        <h1>
-            <a href="/posts/<?= $post->slug; ?>">
-                <?= $post->title ?>
-            </a>
-        </h1>
-        <div>
-            <?= $post->excerpt ?>
-        </div>
-    </article>
-    <?php endforeach; ?>
+    @foreach ($posts as $post)
+        <article class="{{ $loop->even ? 'foobar' : '' }}">
+            <h1>
+                <a href="/posts/{{ $post->slug }}">
+
+                    {{ $post->title }}
+                </a>
+            </h1>
+            <div>
+                {{ $post->excerpt }}
+            </div>
+        </article>
+    @endforeach
 </body>
