58c9d94aecda1c5b15daed989536b40735ca313c
sonnenbergmarc <msonnenberg@live.com>
2023-12-18 05:55:42 -0700

    Created register form with csrf and hash password

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -3,9 +3,13 @@
 // use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('register', [RegisterController::class, 'create']);
+Route::post('register', [RegisterController::class, 'store']);
 
 // Route::get('categories/{category:slug}', function (Category $category) {
 //     return view('posts', [
