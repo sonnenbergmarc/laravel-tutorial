c6870b19373e1fee12198c210369f43abf4bc903
sonnenbergmarc <msonnenberg@live.com>
2023-12-18 08:11:01 -0700

    Having different header for guest and auth and logout button

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -4,12 +4,14 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionsController;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
-Route::get('register', [RegisterController::class, 'create']);
-Route::post('register', [RegisterController::class, 'store']);
+Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
+Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
+Route::post('logout', [SessionsController::class, 'destroy']);
 
 // Route::get('categories/{category:slug}', function (Category $category) {
 //     return view('posts', [
