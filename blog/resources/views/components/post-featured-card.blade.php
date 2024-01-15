9e23aef852a77dd054b66f733ecfb10d3bb67da9
sonnenbergmarc <msonnenberg@live.com>
2024-01-15 11:32:42 -0700

    Post creation form and also added a thumbnail upload

--- blog/resources/views/components/post-featured-card.blade.php
+++ blog/resources/views/components/post-featured-card.blade.php
@@ -4,7 +4,7 @@
     <div class="py-6 px-5 lg:flex">
         <div class="flex-1 lg:mr-8">
             <!-- TODO -->
-            <img src="/images/illustration-1.png" alt="Blog Post illustration" class="rounded-xl">
+            <img src="{{ asset('storage/' . $post->thumbnail) }}" alt="Blog Post illustration" class="rounded-xl">
         </div>
 
         <div class="flex-1 flex flex-col justify-between">
@@ -14,7 +14,7 @@
                 </div>
 
                 <div class="mt-4">
-                    <h1 class="text-3xl">   
+                    <h1 class="text-3xl">
                         <a href="/posts/{{ $post->slug }}">
                         {{ $post->title }}
                         </a>
--- blog/resources/views/posts/create.blade.php
+++ blog/resources/views/posts/create.blade.php
@@ -1,18 +1,52 @@
 <x-layout>
-    <section class="px-6 py-8">
-        <x-panel class="max-w-sm mx-auto">
-            <form action="/admin/posts" method="POST">
+    <section class="py-8 max-w-md mx-auto">
+        <h1 class="text-lg font-bold mb-4">
+            Publish New Post
+        </h1>
+        <x-panel>
+            <form action="/admin/posts" method="POST" enctype="multipart/form-data">
                 @csrf
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="title">
                         Title
                     </label>
-                    <input class="border border-gray-400 p-2 w-full" type="text" name="title" id="title" required>
+                    <input class="border border-gray-400 p-2 w-full" type="text" name="title"
+                    id="title"
+                    value="{{ old('title') }}"
+                    required>
                     @error('title')
                         <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
                     @enderror
                 </div>
 
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="slug">
+                        Slug
+                    </label>
+                    <input class="border border-gray-400 p-2 w-full"
+                        type="text"
+                        name="slug"
+                        id="slug"
+                        value="{{ old('slug') }}"
+                        required>
+                    @error('slug')
+                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="thumbnail">
+                        Thumbnail
+                    </label>
+                    <input class="border border-gray-400 p-2 w-full"
+                        type="file"
+                        name="thumbnail"
+                        id="thumbnail"
+                        required>
+                    @error('thumbnail')
+                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                    @enderror
+                </div>
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="excerpt">
                         Excerpt
@@ -20,8 +54,7 @@
                     <textarea class="border border-gray-400 p-2 w-full"
                         name="excerpt"
                         id="excerpt"
-                        required>
-                    </textarea>
+                        required>{{ old('excerpt') }}</textarea>
                     @error('excerpt')
                         <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
                     @enderror
@@ -34,8 +67,7 @@
                     <textarea class="border border-gray-400 p-2 w-full"
                         name="body"
                         id="body"
-                        required>
-                    </textarea>
+                        required> {{ old('body') }}</textarea>
                     @error('body')
                         <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
                     @enderror
@@ -52,7 +84,9 @@
                         @endphp --}}
 
                         @foreach (\App\Models\Category::all() as $category)
-                            <option value="{{ $category->id }}">{{ ucwords($category->name) }}</option>
+                            <option value="{{ $category->id }}"
+                                {{ old('category_id') == $category->id ? 'selected' : ''}}
+                                >{{ ucwords($category->name) }}</option>
                         @endforeach
                     </select>
 
