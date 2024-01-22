627a668edeb6be7e2458e750010de5c05081637f
sonnenbergmarc <msonnenberg@live.com>
2024-01-22 05:05:32 -0700

    Changing more form items into components

--- blog/resources/views/components/form/input.blade.php
+++ blog/resources/views/components/form/input.blade.php
@@ -1,12 +1,9 @@
 @props(['name', 'type' => 'text'])
 
-<div class="mb-6">
-    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="{{$name}}">
-        {{ ucwords($name) }}
-    </label>
+<x-form.field>
+    <x-form.label name="{{ $name }}" />
+
     <input class="border border-gray-400 p-2 w-full" type="{{$type}}" name="{{$name}}" id="{{$name}}" value="{{ old($name) }}"
         required>
-    @error($name)
-        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-    @enderror
-</div>
+    <x-form.error name="{{ $name }}" />
+</x-form.field>
--- blog/resources/views/posts/create.blade.php
+++ blog/resources/views/posts/create.blade.php
@@ -11,27 +11,10 @@
                 <x-form.input name="slug" />
                 <x-form.input name="thumbnail" type="file" />
                 <x-form.textarea name="excerpt" />
+                <x-form.textarea name="body" />
 
-
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="body">
-                        Body
-                    </label>
-                    <textarea class="border border-gray-400 p-2 w-full"
-                        name="body"
-                        id="body"
-                        required> {{ old('body') }}</textarea>
-                    @error('body')
-                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="category_id">
-                        Category
-                    </label>
-
+                <x-form.field>
+                    <x-form.label name="category" />
                     <select name="category_id" id="category_id">
                         {{-- @php
                             $categories = \App\Models\Category::all();
@@ -43,12 +26,9 @@
                                 >{{ ucwords($category->name) }}</option>
                         @endforeach
                     </select>
-
-                    @error('category')
-                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
-                <x-submit-button>Publish</x-submit-button>
+                    <x-form.error name="category" />
+                </x-form.field>
+                <x-form.button>Publish</x-form.button>
             </form>
         </x-panel>
     </section>
