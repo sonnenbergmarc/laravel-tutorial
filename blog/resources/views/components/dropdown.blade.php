2fb6bf75d95996413f67242ce2010894762a7e70
sonnenbergmarc <msonnenberg@live.com>
2024-01-22 05:18:49 -0700

    Creating a dropdown menu for the admin dashboard, new post and logout

--- blog/resources/views/components/dropdown.blade.php
+++ blog/resources/views/components/dropdown.blade.php
@@ -1,6 +1,6 @@
 @props(['trigger'])
 
-<div x-data="{ show: false }" @click.outside="show = false">
+<div x-data="{ show: false }" @click.outside="show = false" class="relative">
     <!-- Trigger -->
         <div @click="show = ! show">
             {{ $trigger }}
