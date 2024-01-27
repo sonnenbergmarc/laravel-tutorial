ed1b8978fb52a6249314ee51f3902bb7b8498719
sonnenbergmarc <msonnenberg@live.com>
2024-01-27 09:27:45 -0700

    Added a sidebar in new posts, completed dropdown items, set active links, changed forms to new components

--- blog/resources/views/register/create.blade.php
+++ blog/resources/views/register/create.blade.php
@@ -6,78 +6,13 @@
             <form method="POST" action="/register" class="mt-10">
                 @csrf
 
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="name">
-                        Name
-                    </label>
-                    <input  class="border border-gray-400 p-2 w-full"
-                        type="text"
-                        name="name"
-                        id="name"
-                        value="{{ old('name') }}"
-                        required>
+                <x-form.input name="name" />
+                <x-form.input name="username" />
+                <x-form.input name="email" type="email" autocomplete="username" />
+                <x-form.input name="password" type="password" autocomplete="new-password" />
 
-                    @error('name')
-                        <p class="text-red-500 text-xs mt-1">
-                            {{ $message }}
-                        </p>
-                    @enderror
-                </div>
 
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="username">
-                        Username
-                    </label>
-                    <input  class="border border-gray-400 p-2 w-full"
-                        type="text"
-                        name="username"
-                        id="username"
-                        value="{{ old('username') }}"
-                        required>
-                        @error('username')
-                        <p class="text-red-500 text-xs mt-1">
-                            {{ $message }}
-                        </p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="email">
-                        Email
-                    </label>
-                    <input class="border border-gray-400 p-2 w-full"
-                        type="email"
-                        name="email"
-                        id="email"
-                        value="{{ old('email') }}"
-                        required>
-                        @error('email')
-                        <p class="text-red-500 text-xs mt-1">
-                            {{ $message }}
-                        </p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="password">
-                        Password
-                    </label>
-                    <input  class="border border-gray-400 p-2 w-full"
-                        type="password"
-                        name="password"
-                        id="password"
-                        required>
-                        @error('password')
-                        <p class="text-red-500 text-xs mt-1">
-                            {{ $message }}
-                        </p>
-                    @enderror
-                </div>
-                <div class="mb-6">
-                    <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">
-                        Submit
-                    </button>
-                </div>
+                <x-form.button>Submit</x-form.button>
 
                 {{-- @if ($errors->any())
                     <ul>
